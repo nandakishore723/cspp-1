818a40901fdd177e8ac63c2aacd4590b550a5b90
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-04 22:30:40 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/perfect_cube.py
@@ -2,26 +2,24 @@
 @author : nandakishore723
 Write a python program to find if the given number is a perfect cube or not
 using guess and check algorithm
-
 testcase 1
 Input: 24389
 Output: 24389 is a perfect cube
-
 testcase 2
 Input: 21950
 Output: 21950 is not a perfect cube
 '''
-
 def main():
-	ep = 0.01
-	step = 0.1
-	sq = int(input())
-	gu = 0.0
-	while gu <= sq:
-		if abs(gu**2-sq) < ep:
-			break
-	else:
-		gu = gu + step
-print(str(gu))
-if __name__== "__main__":
-	main()
+    '''
+    finding perfect cube or not
+    '''
+    cu_a = int(input())
+    gu_a = 1
+    while (gu_a**3) < cu_a:
+        gu_a = gu_a + 1
+        if int(gu_a**3) == cu_a:
+            print(cu_a, 'is perfect cube')
+        else:
+            print(cu_a, 'is not perfect cube')
+if __name__ == "__main__":
+    main()
--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -1,21 +1,27 @@
-# Write a python program to find the square root of the given number
-# using approximation method
-
-# testcase 1
-# input: 25
-# output: 4.999999999999998
-
-# testcase 2
-# input: 49
-# output: 6.999999999999991
-
+'''
+@author : nandakishore723
+Write a python program to find the square root of the given number
+using approximation method
+testcase 1
+input: 25
+output: 4.999999999999998
+testcase 2
+input: 49
+output: 6.999999999999991
+'''
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''
+    finding square root of the number
+    '''
+    epsilon = 0.01
+    step = 0.1
+    square = 0.1
+    guess = 0.0
+    while guess <= square:
+        if abs(guess**2-square) < epsilon:
+            break
+        else:
+            guess = guess + step
+            print(str(guess))
+if __name__ == "__main__":
+    main()
