ca2fe96c3c7a4815d81a600c09ee8d6854a62475
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-16 11:04:47 +0530

    " -> 3 of 3 passed. pylint: 9.12/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -3,7 +3,7 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-d_cards = {'A':14, 'K':13, 'Q':12, 'J':11, '10':10, '9':9, '8':8, '7':7, \
+d_cards = {'A':14, 'K':13, 'Q':12, 'J':11, 'T':10, '9':9, '8':8, '7':7, \
 '6':6, '5':5, '4':4, '3':3, '2':2}
 
 def is_straight(hand):
