922d81d03b00a4a031f86e6fb3a43a196e04a42a
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-17 21:54:51 +0530

    " -> 1 of 19 passed. pylint: 4.20/10 "

--- cspp1-assignments/m15/CodeCampPoker/poker3.py
+++ cspp1-assignments/m15/CodeCampPoker/poker3.py
@@ -40,34 +40,36 @@ def is_flush(hand):
 
 def four_ofakind(hand):
     '''when four of same kind and one other of other rank
+    card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
+    return len(card_values) == 2
     '''
-
-    card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
-    return len(card_values) == 2
+    return len(set[hand]) == 2
 def full_house(hand):
+    '''
     card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
     return len(card_values) == 2
-
+    '''
+    return len(set[hand]) == 2
 def three_ofakind(hand):
     '''when three of same kind and two of other of other rank
-    '''
-
     card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
     return len(card_values) == 3
-
+    '''
+    return len(set[hand]) == 3
 def two_pair_ofakind(hand):
     '''when two of same kind and three other of other rank
-    '''
-
     card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
     return len(card_values) == 3
+    '''
+    return len(set[hand]) == 3
 
 def one_pairofakind(hand):
     '''
     sends bool value if the one pair condition
-    '''
     card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
     return len(card_values) == 4
+    '''
+    return len(set[hand]) == 4
 
 def hand_rank(hand):
     '''
