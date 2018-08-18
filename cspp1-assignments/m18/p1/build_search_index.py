bb1610e5a8eae9a6c8f336b4552bcf090d56e65d
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-18 12:05:03 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -83,7 +83,7 @@ def build_search_index(docs):
             dictionary[word] = docs[word]
     for word in docs:
         if word not in dictionary:
-            dictionary[word] = [0, docs[word]]
+            dictionary[word] = [docs[word], 0]
     return dictionary
 
 # helper function to print the search index
