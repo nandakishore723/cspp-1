0780f92c1ac43855e32b4a115c1c557124ab4645
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-25 13:00:44 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment4/print_dictionary.py
+++ cspp1-assignments/m22/assignment4/print_dictionary.py
@@ -5,14 +5,13 @@ frequency of each word. Format of the printing should be one key per line and se
 the key and frequency with a SPACE - SPACE.
 '''
 def print_dictionary(dictionary):
-    """print dictionary"""
-   
+ """printing dictionary"""
     keys = sorted(dictionary.keys())
     for key in keys:
         print(key, "-", dictionary[key])
 
 def main():
-    """this is main"""
+"""it's main"""
     dictionary = eval(input())
     print_dictionary(dictionary)
 if __name__ == '__main__':
