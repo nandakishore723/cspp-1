2af32ed856dd57a622c2102849898a7a38c1ac09
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-25 13:07:15 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -5,12 +5,13 @@ characters and retain alphabets in both upper and lower case and numbers.
 '''
 import re
 #import math
-def clean_string(string):
-	
-    return re.sub('[^a-zA-Z0-9]', '', string)
+def clean_string(str_a):
+
+    return re.sub('[^a-zA-Z0-9]', '', str_a)
 
 def main():
+	'''This is main function'''
     string = input()
-    print(clean_string(string))
+    print(clean_string(str_a))
 if __name__ == '__main__':
     main()
