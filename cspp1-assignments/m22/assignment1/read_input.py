407dc062914cd112f68dd816611183d1ce7565d1
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-25 11:03:20 +0530

    " -> 0 of 2 passed. pylint: -7.00/10 "

--- cspp1-assignments/m22/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/read_input.py
@@ -2,9 +2,14 @@
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
 def ss(s):
-    lines = ""
-    for i in s:
-        lines += input()+"\n"
+
+ #   lines = ""
+  #  for i in s:
+    s = ''
+    lines = []
+    [lines.append(line) for line in text]
+
+print(lines)
 def main():
     s = input()
     ss(s)
