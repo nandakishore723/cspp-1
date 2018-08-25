bd6735811a8df59662b5faca17e03d763e94dc72
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-25 13:15:55 +0530

    " -> 4 of 4 passed. pylint: 9.23/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -11,24 +11,25 @@
 def next_cell(grid,i,j):
     for val in range(i,9):
         for val in range(j,9):
-            if grid[x][y] == 0:
-                return x,y
+            if grid[a][b] == 0:
+                return a,b
 
     for val in range(0,9):
         for val in range(0,9):
-            if grid[x][y] == 0:
+            if grid[a][b] == 0:
     return -1 -1
 
 def if_valid(grid,i,j,e):
     row = all([e != grid[i][x] for x in range(9)])
     if row:
         column = all([e != grid[x][j] for x in range(9)])
+        if column:
+
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
-    '''
-    
+'''
 
 def main():
     '''
--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -1,14 +1,22 @@
 '''
+@author : nandakishore723
 Write a function to print a dictionary with the keys in sorted order along with the
 frequency of each word. Display the frequency values using “#” as a text based graph
 '''
-
-def frequency_graph(dictionary):
-    pass
+def freq_graph(dictionary):
+    """frequency graph"""
+    keys = sorted(dictionary.keys())
+    for key in keys:
+        var = ""
+        for i in range(dictionary[key]):
+            var += '#'
+            i = i + 1
+        print(key, '-', var)
 
 def main():
+    """main"""
     dictionary = eval(input())
-    frequency_graph(dictionary)
+    freq_graph(dictionary)
 
 if __name__ == '__main__':
     main()
