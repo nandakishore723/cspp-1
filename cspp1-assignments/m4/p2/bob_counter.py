a7a18d3d8b5fd1fb423d7bb0ea87e46fdd7b66cb
nandakishore723 <nandakishore723@msitprogram.net>
2019-08-23 07:02:14 +0530

    " -> 0 of 5 passed. pylint: 8.89/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -3,23 +3,28 @@ Assume s is a string of lower case characters.
 Write a program that prints the number of times the string 'bob' occurs in s.
 For example, if s = 'azcbobobegghakl', then your program should print
 Number of times bob occurs is: 2'''
-
+import re
 def main():
     '''Write a program that prints the number of times the string 'bob' occurs
     in s.'''
     string = input()
-    string_to_find = 'bob'
     count = 0
-    for i in range(len(string)-2):
-        b_c = 0
-        n_r = 0
-        o_r = i
-        while (n_r < 3 and string[o_r] == string_to_find[n_r]):
-            b_c = b_c + 1
-            n_r = n_r + 1
-            o_r = o_r + 1
-        if b_c == 3:
-            count = count + 1
-    print(count)
+    # string_to_find = 'bob'
+    # count = 0
+    # for i in range(len(string)-2):
+    #     b_c = 0
+    #     n_r = 0
+    #     o_r = i
+    #     while (n_r < 3 and string[o_r] == string_to_find[n_r]):
+    #         b_c = b_c + 1
+    #         n_r = n_r + 1
+    #         o_r = o_r + 1
+    #     if b_c == 3:
+    #         count = count + 1
+    # print(count)
+    p = re.compile('bob')
+    if p.findall(string):
+        count += 1
+
 if __name__ == "__main__":
     main()
--- video_files/m2/nestedConds.py
+++ video_files/m2/nestedConds.py
@@ -1,14 +1,78 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun  8 11:07:33 2016
+# # -*- coding: utf-8 -*-
+# """
+# Created on Wed Jun  8 11:07:33 2016
 
-@author: ericgrimson
-"""
+# @author: ericgrimson
+# """
 
-if x%2 == 0:
-    if x%3 == 0:
-        print('Divisible by 2 and 3')
-    else:
-        print('Divisible by 2 and not by 3')
-elif x%3 == 0:
-    print('Divisible by 3 and not by 2')
+# if x%2 == 0:
+#     if x%3 == 0:
+#         print('Divisible by 2 and 3')
+#     else:
+#         print('Divisible by 2 and not by 3')
+# elif x%3 == 0:
+#     print('Divisible by 3 and not by 2')
+# import operator
+# t = ()
+# print(t)
+# t = ('me','u')
+# print(t)
+# print(t)
+# s= set([1,2,2,3,4,4,5,'me',6])
+# print(s)
+# d = {}
+# print(d)
+# d = dict([(1, 'me'),(2,'u')])
+# print(d)
+# d = {1:'me',2:'u'}
+# print(d)
+# d = {1:'me',2:'u',3:{1:'me',2:'u'}}
+# print(d)
+# d1 = d.copy()
+# print(d1)
+# #type(d1)
+# print(type(d1))
+
+# x = 100
+# y = 4000
+# x, y = y, x
+# print(x,y)
+
+# a = 4; b =2
+# print(a+b)
+# print(a*b)
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+
+# print(a is not b)
+# max = a if a>b else b
+# print(max)
+# print(any ([False,False]))
+# print(any ([False,False,True]))
+# print(any ([True,False,False]))
+# print(all ([False,False]))
+# print(all ([False,False,True]))
+# print(all ([True,True]))
+
+# print(operator.add(a, b))
+# print(operator.sub(a, b))
+# print(operator.mul(a, b))
+# print(operator.truediv(a, b))
+# print(operator.floordiv(a, b))
+# print(operator.mod(a, b))
+
+# import array
+# count = 0
+# arr1 = array.array('i', [1,2,2,3,5,8])
+# print(arr1.count(2))
+
+# for _ in range(0,6):
+# 	print(arr1[_], end = " ")
+
+import re
+p = re.compile('\d+')
+print(p.findall("rabcdeefgyYhFjkIoomnpOeorteeeeet 11 22222-1256-226-1-0-2"))
